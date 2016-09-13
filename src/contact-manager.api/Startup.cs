4916ea5f9b366df2d46b448a942aa82051e5483c
Radu Matei <matei.radu94@gmail.com>
2016-09-13 05:40:15 +0300

    Updated Startup
    - updated services
    - configured Mvc

--- src/contact-manager.api/Startup.cs
+++ src/contact-manager.api/Startup.cs
@@ -1,9 +1,22 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 public class Startup
 {
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+        services.AddSingleton<IContactRepsository, InMemoryContactRepository>();
+    }
+
     public void Configure(IApplicationBuilder app)
     {
-        
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+                name: "default",
+                template: "api/{controller}/{action}/{id?}"
+            );
+        });
     }
 }
