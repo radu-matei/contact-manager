e2d1a6b55157ff5cec4ab5cebcfa9c9f4424a58f
Radu Matei <matei.radu94@gmail.com>
2016-09-13 14:16:45 +0300

    Added contact-detail component

---
+++ src/contact-manager.web/src/contact-detail.ts
@@ -0,0 +1,47 @@
+import {inject} from 'aurelia-framework';
+import {WebAPI} from './web-api';
+import {areEqual} from './utility';
+
+interface Contact {
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
+@inject(WebAPI)
+export class ContactDetail {
+    routeConfig;
+    contact: Contact;
+    originalContact: Contact;
+
+    constructor(private api: WebAPI){ }
+
+    activate(params, routeConfig){
+        this.routeConfig = routeConfig;
+
+        return this.api.getContactDetails(params.id).then((contact: Contact) => {
+            this.contact = contact;
+            this.routeConfig.navModel.setTitle(this.contact.firstName);
+            this.originalContact = JSON.parse(JSON.stringify(this.contact));
+        });
+    }
+
+    get canSave(){
+        return this.contact.firstName && this.contact.lastName && !this.api.isRequesting;
+    }
+
+    save() {
+    this.api.saveContact(this.contact).then(contact => {
+      this.contact = <Contact>contact;
+      this.routeConfig.navModel.setTitle(this.contact.firstName);
+      this.originalContact = JSON.parse(JSON.stringify(contact));
+    });
+  }
+
+    canDeactivate() {
+    if (!areEqual(this.originalContact, this.contact)) {
+      return confirm('You have unsaved changes. Are you sure you wish to leave?');
+    }
+    return true;
+    }
+}
