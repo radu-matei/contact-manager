ab03afc0d7681bd1dcc05dcd78a833668ca939cf
Radu Matei <matei.radu94@gmail.com>
2016-09-28 14:44:16 +0300

    Implemented SignalR base component

--- src/contact-manager.web/src/signalr-component.ts
+++ src/contact-manager.web/src/signalr-component.ts
@@ -1,26 +1,71 @@
 import { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
 import * as $ from 'jquery';
 import 'ms-signalr-client';
+import { ConnectionMessages } from './utils';
 
+@inject(EventAggregator, ConnectionMessages)
 export class SignalRConnection {
 
-    constructor() {
+    private channelName = "contactsHub";
+    protected eventAggregator: EventAggregator;
+    protected clientMethods: string[] = ["contactUpdated"];
+    protected isConnected: boolean = false;
+    protected hubProxy: any;
+    protected connection: any;
+    protected connectionMessages: ConnectionMessages;
 
+    constructor(eventAggregator: EventAggregator, connectionMessages: ConnectionMessages) {
+        this.eventAggregator = eventAggregator;
+        this.connectionMessages = connectionMessages;
+
+        this.configureConnection();
+        this.subscribeToConnectionEvents();
     }
 
-    tst() {
+    protected configureConnection(): void {
         var url = "http://localhost:5000/signalr";
 
-        var connection = $.hubConnection(url);
-        var contosoChatHubProxy = connection.createHubProxy('contactsHub');
+        this.connection = $.hubConnection(url);
+        this.hubProxy = this.connection.createHubProxy('contactsHub');
 
-        contosoChatHubProxy.on('updateContact', function (contact) {
-            console.log(contact.firstName + contact.lastName);
+    }
+
+    protected subscribeToConnectionEvents(): void {
+        this.eventAggregator.subscribe(this.channelName, (connectionMessage) => {
+            if (connectionMessage == this.connectionMessages.subscribe)
+                this.subscribe();
+            else
+                if (connectionMessage == this.connectionMessages.unsubscribe)
+                    this.unsubscribe();
         });
+    }
 
-        $.connection.hub.logging = true;
-        connection.start({jsonp:true})
-            .done(function () { console.log('Now connected, connection ID=' + connection.id); })
-            .fail(function () { console.log('Could not connect'); });
+    protected configureClientMethods(): void {
+        for (var method in this.clientMethods) {
+            this.hubProxy.on(this.clientMethods[method], this.publishClientMethod(this.clientMethods[method]));
         }
     }
+
+    private publishClientMethod(method: string): Function {
+        return (object) => { this.eventAggregator.publish(this.getMethodChannel(method), object); };
+    }
+
+    private getMethodChannel(methodName: string): string {
+        return this.channelName + "-" + methodName;
+    }
+
+    public subscribe(): void {
+        this.configureClientMethods();
+
+        if (this.isConnected == false) {
+            this.connection.start({ jsonp: true })
+                .done(function () { console.log('Now connected'); })
+                .fail(function () { console.log('Could not connect'); });
+        }
+    }
+
+    public unsubscribe(): void {
+        $.connection.hub.stop();
+    }
+}
