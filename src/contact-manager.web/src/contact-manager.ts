dfcd411fcd972640cdd3bb46fa419c582d1f0712
Radu Matei <matei.radu94@gmail.com>
2016-09-28 06:39:22 +0300

    Added update contact call on contact manager

--- src/contact-manager.web/src/contact-manager.ts
+++ src/contact-manager.web/src/contact-manager.ts
@@ -1,23 +1,32 @@
-import {HttpClient, json} from 'aurelia-fetch-client';
-import {inject} from 'aurelia-framework';
+import { HttpClient, json } from 'aurelia-fetch-client';
+import { inject } from 'aurelia-framework';
+import { Contact } from './interfaces';
 
 @inject(HttpClient)
 export class ContactManager {
-  httpClient: HttpClient;
+    httpClient: HttpClient;
 
-  constructor(httpClient: HttpClient){
-      this.httpClient = httpClient; 
-  }
+    constructor(httpClient: HttpClient) {
+        this.httpClient = httpClient;
+    }
 
-  public getContacts() {
-    return this.httpClient.fetch('http://localhost:5000/api/Contacts/GetContacts')
-                          .then(response => response.json())
-  }
+    public getContacts() {
+        return this.httpClient.fetch('http://localhost:5000/api/Contacts/GetContacts')
+            .then(response => response.json())
+    }
 
-  public getContactById(id: number){
-      let path = 'http://localhost:5000/api/Contacts/GetContactById?id='+id;
+    public getContactById(id: number) {
+        let path = 'http://localhost:5000/api/Contacts/GetContactById?id=' + id;
 
-      return this.httpClient.fetch(path)
-                 .then(response => response.json())
-  }
+        return this.httpClient.fetch(path)
+            .then(response => response.json())
+    }
+
+    public updateContact(contact: Contact) {
+
+        this.httpClient.fetch('http://localhost:5000/api/Contacts/UpdateContact', {
+            method: 'post',
+            body: json(contact)
+        });
+    }
 }
