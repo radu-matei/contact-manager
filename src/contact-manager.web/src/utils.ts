d9762e8c00da0356a04426208d4b2ce5e2a4401d
Radu Matei <matei.radu94@gmail.com>
2016-09-28 14:44:44 +0300

    Renamed interfaces to utils

---
+++ src/contact-manager.web/src/utils.ts
@@ -0,0 +1,28 @@
+export interface Contact {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phoneNumber: string;
+}
+
+export interface ContactsClient {
+    firstNameChanged(firstName: string): void;
+}
+
+export interface ContactsProxy {
+    client: ContactsClient;
+}
+
+export interface SignalR {
+    contactsHub: ContactsProxy;
+}
+
+
+export interface JQuery {
+    connection: any;
+}
+
+export class ConnectionMessages {
+    subscribe: string = "subscribe";
+    unsubscribe: string = "unsubscribe";
+};
