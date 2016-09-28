1b414858ba6121d7a44637ee4032890fdee2fd4a
Radu Matei <matei.radu94@gmail.com>
2016-09-28 06:39:38 +0300

    Added update contact logic on contact details

--- src/contact-manager.web/src/contact-detail.ts
+++ src/contact-manager.web/src/contact-detail.ts
@@ -8,6 +8,7 @@ export class ContactDetail {
   routeConfig;
   contactManager: ContactManager;
   contact: Contact;
+  originalContact: Contact;
 
   constructor(contactManager: ContactManager) {
     this.contactManager = contactManager;
@@ -16,10 +17,22 @@ export class ContactDetail {
   activate(params, routeConfig) {
     this.routeConfig = routeConfig;
 
-    return this.contactManager.getContactById(params.id).then((contact: Contact) => {
-      this.contact = contact;
+    return this.contactManager.getContactById(params.id).then((c: Contact) => {
+      this.contact = c;
+      this.originalContact = JSON.parse(JSON.stringify(this.contact));
+      
       this.routeConfig.navModel.setTitle(this.contact.firstName);
     });
   }
 
+  get canSave(): boolean {
+      return this.originalContact.firstName != this.contact.firstName ||
+             this.originalContact.lastName != this.contact.lastName ||
+             this.originalContact.email != this.contact.email ||
+             this.originalContact.phoneNumber != this.contact.phoneNumber;
+  }
+
+  save(): void {
+      this.contactManager.updateContact(this.contact);
+  }
 }
