05290d8a01956700e3152a75345fe4519899b97d
Radu Matei <matei.radu94@gmail.com>
2016-09-28 06:38:52 +0300

    Added CORS options to allow specific origin

--- src/contact-manager.api/Startup.cs
+++ src/contact-manager.api/Startup.cs
@@ -1,20 +1,32 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
 using Microsoft.Extensions.DependencyInjection;
 
 public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                                  builder => builder.WithOrigins("http://localhost:9000")
+                                                    .AllowAnyHeader()
+                                                    .AllowAnyMethod());
+            });
         services.AddMvc();
+        services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowSpecificOrigin"));
+            });
+
+
         services.AddSingleton<IContactRepsository, InMemoryContactRepository>();
-        services.AddCors();
     }
 
     public void Configure(IApplicationBuilder app)
     {
-
-    app.UseCors(builder =>
-       builder.WithOrigins("http://localhost:9000"));
+        app.UseCors("AllowSpecificOrigin");
 
         app.UseMvc(routes =>
         {
