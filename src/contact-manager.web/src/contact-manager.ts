8846c6cf736ab9acc94530e6ee0713023d3b00ce
Radu Matei <matei.radu94@gmail.com>
2016-09-28 04:36:55 +0300

    Added ContactManager method to get details

--- src/contact-manager.web/src/contact-manager.ts
+++ src/contact-manager.web/src/contact-manager.ts
@@ -5,15 +5,19 @@ import {inject} from 'aurelia-framework';
 export class ContactManager {
   httpClient: HttpClient;
 
-
   constructor(httpClient: HttpClient){
-      this.httpClient = httpClient;
-
-      
+      this.httpClient = httpClient; 
   }
 
   public getContacts() {
     return this.httpClient.fetch('http://localhost:5000/api/Contacts/GetContacts')
-      .then(response => response.json())
+                          .then(response => response.json())
+  }
+
+  public getContactById(id: number){
+      let path = 'http://localhost:5000/api/Contacts/GetContactById?id='+id;
+
+      return this.httpClient.fetch(path)
+                 .then(response => response.json())
   }
 }
