e86aad46f13f75c8385e043b622ffa8395004ca7
Radu Matei <matei.radu94@gmail.com>
2016-09-28 08:47:54 +0300

    Added basic signals interfaces

--- src/contact-manager.web/src/interfaces.ts
+++ src/contact-manager.web/src/interfaces.ts
@@ -5,3 +5,19 @@ export interface Contact {
     phoneNumber: string;
 }
 
+    export interface ContactsClient {
+        firstNameChanged(firstName: string): void;
+    }
+
+    export interface ContactsProxy {
+        client: ContactsClient;
+    }
+
+    export interface SignalR {
+        contactsHub: ContactsProxy;
+    }
+
+
+export interface JQuery {
+    connection: any;
+}
