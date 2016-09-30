45c3975de7af7d4615bbe71849158dba680b0fce
Radu Matei <matei.radu94@gmail.com>
2016-09-30 11:47:47 +0300

    Added more elements in the contact list

--- src/contact-manager.api/InMemoryContactRepository.cs
+++ src/contact-manager.api/InMemoryContactRepository.cs
@@ -7,7 +7,9 @@ public class InMemoryContactRepository : IContactRepsository
     private List<Contact> _contacts = new List<Contact>()
     {
         new Contact("Obi-Wan", "Kenobi", "obi@wan-kenobi.com", "1234"),
-        new Contact("Darth", "Vader", "darth@vader.com", "5678")
+        new Contact("Darth", "Vader", "darth@vader.com", "5678"),
+        new Contact("Luke", "Skywalker", "onehand@luke.com", "511621"),
+        new Contact("C3PO", "the Protocol Droid", "redhand@c.3po", "123")
     };
 
     public void AddContact(Contact contact)
